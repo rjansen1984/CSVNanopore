137851539b3cc841578fb5cc1e006a070c17e23f
rjansen1984 <rick.jansen1984@gmail.com>
2018-03-19 16:38:44 +0100

    Removed subspecies names and added column sorting

--- csv_nanopore.py
+++ csv_nanopore.py
@@ -24,9 +24,9 @@ rank_dict = {"1": "phylum", "2": "class", "3": "order", "4": "family", "5": "gen
 def is_taxadb_up_to_date(dbfile=DEFAULT_TAXADB):
     """
     Check if a valid and up-to-date taxa.sqlite database exists
-    If dbfile is not specified, DEFAULT_TAXADB is assumed    
-    :param dbfile: 
-    :return: 
+    If dbfile is not specified, DEFAULT_TAXADB is assumed
+    :param dbfile:
+    :return:
     """
     db = sqlite3.connect(dbfile)
     try:
@@ -43,9 +43,9 @@ def is_taxadb_up_to_date(dbfile=DEFAULT_TAXADB):
 def get_desired_ranks(taxid, desired_ranks):
     """
     Get rank entered by user for all taxids.
-    :param taxid: 
-    :param desired_ranks: 
-    :return: 
+    :param taxid:
+    :param desired_ranks:
+    :return:
     """
     lineage = ncbi.get_lineage(taxid)
     names = ncbi.get_taxid_translator(lineage)
@@ -57,25 +57,39 @@ def get_desired_ranks(taxid, desired_ranks):
 def read_csv():
     """
     Read the csv files and build lists with available taxids and occurance.
-    :return: 
+    :return:
     """
     count = 0
     for csv in onlyfiles:
         klist = []
         tax = []
         tids = []
+        taxnames = []
         with open(mypath + csv) as nf:
             linenr = 0
             for line in nf:
                 if ',,' not in line:
                     line = line.split(',')
+                    name = ""
                     if linenr > 0:
                         if float(line[3]) >= 80 and line[8] in ['1', '2']:
                             tids.append(line[4])
                             if rank_dict[level] == "species":
-                                tax.append(line[2])
+                                # tax.append(line[2])
+                                taxid2name = ncbi.get_taxid_translator([int(line[2])])
+                                for tid, tname in taxid2name.iteritems():
+                                    namelist = str(tname).split(" ")[:2]
+                                for nl in namelist:
+                                    name += str(nl) + " "
+                                taxnames.append(str(name))
                             elif rank_dict[level] == "genus":
-                                tax.append(line[7])
+                                # tax.append(line[7])
+                                taxid2name = ncbi.get_taxid_translator([int(line[7])])
+                                for tid, tname in taxid2name.iteritems():
+                                    namelist = str(tname).split(" ")[:2]
+                                for nl in namelist:
+                                    name += str(nl) + " "
+                                taxnames.append(str(name))
                             else:
                                 taxids = [line[2]]
                                 desired_ranks = [rank_dict[level]]
@@ -83,10 +97,17 @@ def read_csv():
                                     ranks = get_desired_ranks(taxid, desired_ranks)
                                     for key, rank in ranks.items():
                                         if rank != '<not present>':
-                                            tax.append(rank)
+                                            # tax.append(rank)
+                                            taxid2name = ncbi.get_taxid_translator([int(rank)])
+                                            for tid, tname in taxid2name.iteritems():
+                                                namelist = str(tname).split(" ")[:2]
+                                            for nl in namelist:
+                                                name += str(nl) + " "
+                                            taxnames.append(str(name))
                 linenr += 1
-            counter = Counter(tax)
-            for k, v in counter.iteritems():
+            # counter = Counter(tax)
+            namecounter = Counter(taxnames)
+            for k, v in namecounter.iteritems():
                 if k not in d and count > 0:
                     for c in range(0, count):
                         d[k].append(0)
@@ -96,7 +117,7 @@ def read_csv():
                 if dk not in klist:
                     d[dk].append(0)
             count += 1
-            counter.clear()
+            namecounter.clear()
     for k, v in d.iteritems():
         if len(d[k]) < len(onlyfiles):
             d[k].append(0)
@@ -108,7 +129,7 @@ def make_rank_csv():
     """
     Create a new csv file based on the rank provided by the user.
     The WIMP csv files will be used as input and the rank will be searched using the NCBI taxid translator.
-    :return: 
+    :return:
     """
     print "Creating csv file based on rank...\n"
     with open(rank_dict[level] + '.csv', 'w') as tf:
@@ -125,9 +146,7 @@ def make_rank_csv():
         for k in d.iteritems():
             x = 0
             line = ""
-            taxid2name = ncbi.get_taxid_translator([int(k[0])])
-            for tid, tname in taxid2name.iteritems():
-                line += str(tname)
+            line += str(k[0])
             line += ','
             for v in k[1]:
                 line += str(v)
@@ -202,6 +221,36 @@ def percentage_nanopore(nanopore):
     print "Writing ", output, "... finished"
 
 
+def sort_file(ifilename, ofilename):
+    """
+    Sorting the nanopore and/or illumina files based on the headers in the fieldnames list
+    :param ifilename: Name of the input file with the Nanopore or Illumina info
+    :param ofilename: The sorted output file
+    :return:
+    """
+    ifilename = ifilename
+    ofilename = ofilename
+    with open(ifilename, 'r') as infile, open(ofilename, 'a') as outfile:
+        # output dict needs a list for new column ordering
+        fieldnames = [4186, 4323, 4135, 4322, 5013, 4371, 4343, 4059, 4243, 6015, 4195, 6030,
+                      4278, 5066, 4395, 6119, 4198, 5136, 5028, 4364, 5006, 6043, 5044, 5408,
+                      4282, 4356, 4206, 5145, 5164, 6026, 5418, 4233, 4179, 5186, 4043, 5168,
+                      4294, 5015, 5411, 6009, 6019, 5001, 5067, 5417, 6049, 4280, 6036, 4185,
+                      5027, 4124, 4187, 6045, 6020, 5082, 6003, 5204, 6334, 6014, 5103, 5306,
+                      6107, 5163, 5425, 5304, 5032, 4306, 6035, 5421, 6010, 6074, "C1", "C15",
+                      "EC01"]
+        fields = ['species']
+        for fn in fieldnames:
+            fields.append(str(fn) + '_#')
+            fields.append(str(fn) + '_%')
+        writer = csv.DictWriter(outfile, fieldnames=fields, extrasaction='ignore')
+        # reorder the header first
+        writer.writeheader()
+        for row in csv.DictReader(infile):
+            # writes the reordered rows to the new file
+            writer.writerow(row)
+
+
 if __name__ == '__main__':
     try:
         if is_taxadb_up_to_date(DEFAULT_TAXADB):
@@ -210,6 +259,7 @@ if __name__ == '__main__':
             else:
                 print "Getting " + rank_dict[level] + " from csv files...\n"
             read_csv()
+            sort_file("percentage_species.csv", "species_sorted.csv")
         else:
             print "Taxonomy database is updating...\n"
             ncbi.update_taxonomy_database()
